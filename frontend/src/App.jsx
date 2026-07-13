b585eef51bb8630462cb6d289ebb5930cf231a28
Javaria Rizwan <javariarizwan332@gmail.com>
2026-07-13 16:26:39 +0500

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Nav'
+import Body from './components/Body'
+
+
+function App() {
+  return (
+    <>
+<Navbar />
+
+
+<Body />
+
+<Footer />
+    </>
+  )
+}
+
+export default App
